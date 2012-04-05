443f371baa53b2b8a75ba750e680b43238ec0acb
Nicolas Gramlich <ngramlich@zynga.com>
2012-04-04 17:42:17 -0700

    Changes due to AndEngine changes.

--- src/org/andengine/extension/robotium/example/AndEngineRobotiumExtensionExample.java
+++ src/org/andengine/extension/robotium/example/AndEngineRobotiumExtensionExample.java
@@ -2,11 +2,10 @@ package org.andengine.extension.robotium.example;
 
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
-import org.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.extension.robotium.example.R;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
